73cf664e0de7a7579a45ebcb6b760192dc241fe7
animehub75 <proxima720p@gmail.com>
2025-07-07 18:51:15 +0530

    feat: configure project for vercel deployment

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import path from "path";
+import { fileURLToPath } from "url";
+import { setupVite, log } from "./vite";
 
 const app = express();
 app.use(express.json());
@@ -53,7 +55,12 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    serveStatic(app);
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    app.use(express.static(path.join(__dirname, '../dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../dist/index.html'));
+    });
   }
 
   // ALWAYS serve the app on port 5000
